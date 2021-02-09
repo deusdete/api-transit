c13e9c6091395b8c06f3693c781626cb71ad47c4
Deusdete Ferreira <deuzinh2010@gmail.com>
2021-02-09 17:49:28 -0300

    Add function for confirmation and delete accidents

--- src/controllers/ReportAccidentController.js
+++ src/controllers/ReportAccidentController.js
@@ -48,4 +48,43 @@ module.exports = {
         .json({ error: "Falha ao criar reportar acidante" });
     }
   },
+  async confirmationAccident(req, res) {
+    const 
+    const { idAccident } = req.params;
+    const id_user = req.id_user;
+
+    try {
+      const accidentFields = {
+        image: req.file.filename,
+        description,
+        location,
+        reported_by: id_user,
+      };
+
+      const accident = await ReportAccident.create(accidentFields);
+
+      return res.status(200).json({ accident });
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ error: "Falha ao criar reportar acidante" });
+    }
+  },
+  async deleteAccident(req, res) {
+    const { idAccident } = req.params
+
+    try {
+        
+      if (!await ReportAccident.findById(idAccident)) {
+        return res.status(404).json({ error: 'Acidente não existe' });
+      }
+
+      await ReportAccident.deleteOne({_id: idAccident});
+
+      return res.status(200).json({mensagem: "Acidente apagada com sucesso"})
+    } catch (error) {
+      console.log(error)
+      return res.status(400).json({ error: "Falha ao apagar Acidente" })
+    }
+  },
 };
